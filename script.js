ad6f151ff74f7bed5ab859b3b9cd9f8fca99fd7a
Mateusz762 <85846402+Mateusz762@users.noreply.github.com>
2022-12-25 19:56:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let popup = document.getElementById("popup")
+
+function opeppopup(){
+    popup.classList.remove("clopup-popup");
+    popup.classList.add("opepup-popup");
+}
+function closepopup(){
+    popup.classList.remove("opepup-popup");
+    popup.classList.add("clopup-popup");
+}
